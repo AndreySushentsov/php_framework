0dd6adff23c765300d1a718a9113e9d67c96ef33
andrey sushentsov <p1oneer8527@gmail.com>
2018-01-23 22:58:06 +0500

    pattern registry

---
+++ public/test/classes/Cache.php
@@ -0,0 +1,15 @@
+<?php
+  namespace classes;
+
+
+  /**
+   *
+   */
+  class Cache
+  {
+
+    function __construct()
+    {
+      echo "Cache Calss";
+    }
+  }
---
+++ public/test/classes/Test.php
@@ -0,0 +1,14 @@
+<?php
+  namespace classes;
+
+  /**
+   *
+   */
+  class Test
+  {
+
+    function __construct()
+    {
+      echo "Test Class";
+    }
+  }
